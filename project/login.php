9e00009fde35fed00d9ef9d8857c8ef04c1cef6b
dd482IT <dd482@njit.edu>
2020-10-16 19:26:23 +0000

    Username/Email Functionality Added

--- project/login.php
+++ project/login.php
@@ -1,7 +1,7 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
     <form method="POST">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required/>
+        <label for="email">Email or Username:</label>
+        <input type="text" id="email" name="email" required/>
         <label for="p1">Password:</label>
         <input type="password" id="p1" name="password" required/>
         <input type="submit" name="login" value="Login"/>
@@ -11,28 +11,41 @@
 if (isset($_POST["login"])) {
     $email = null;
     $password = null;
+    $username = null; 
     if (isset($_POST["email"])) {
         $email = $_POST["email"];
     }
     if (isset($_POST["password"])) {
         $password = $_POST["password"];
     }
+    if (isset($_POST["email"])) {
+      $username = $_POST["email"];
+    }
     $isValid = true;
-    if (!isset($email) || !isset($password)) {
+    if (!isset($email) || !isset($password) || !isset($username)) {
         $isValid = false;
-        flash("Email or password missing");
+        flash("Email/Username or password missing");
     }
     if (!strpos($email, "@")) {
-        $isValid = false;
+        $email = null; 
         //echo "<br>Invalid email<br>";
-        flash("Invalid email");
     }
+
     if ($isValid) {
         $db = getDB();
         if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email LIMIT 1");
-
-            $params = array(":email" => $email);
+            
+            if(isset($email)){
+                $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email LIMIT 1");
+            }elseif(isset($username)){
+            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE username = :username LIMIT 1");
+            }
+            
+            if(isset($email)){
+              $params = array(":email" => $email);
+            }elseif(isset($username)){
+              $params = array(":username" => $username);
+            }
             $r = $stmt->execute($params);
             //echo "db returned: " . var_export($r, true);
             $e = $stmt->errorInfo();
