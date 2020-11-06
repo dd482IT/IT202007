204870fc3d93d54a8f1b94fb96ed4c2bebaf6905
dd482IT <dd482@njit.edu>
2020-11-06 12:40:25 -0500

    Fix

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -12,8 +12,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
-            <li><a href="test_create_accounts.php">Create Egg</a></li>
-            <li><a href="test_list_accounts.php">View Eggs</a></li>
+            <li><a href="test_create_accounts.php">Create Account</a></li>
+            <li><a href="test_list_accounts.php">View Accounts</a></li>
         <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
