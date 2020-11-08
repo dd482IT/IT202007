3d843152352c9176c3d7dc1731a30c1c1363ee4d
Daniel Daszkiewicz <daniel@Nesspresso.local>
2020-11-08 01:15:38 -0500

    Project Update

---
+++ project/test_list_transcations.php
@@ -0,0 +1,61 @@
+
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+$query = "";
+$results = [];
+if (isset($_POST["query"])) {
+    $query = $_POST["query"];
+}
+if (isset($_POST["search"]) && !empty($query)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT Transactions.id, Accounts.account_number as Accounts, Users.username from Transactions as Trans JOIN Users on Account.user_id = Users.id 
+    LEFT JOIN Accounts as Account on Transactions.id = Accounts.id WHERE Transactions.id like :q LIMIT 10");
+    $r = $stmt->execute([":q" => "%$query%"]);
+    if ($r) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results " . var_export($stmt->errorInfo(), true));
+    }
+}
+?>
+<h3>List Incubators</h3>
+<form method="POST">
+    <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="Search" name="search"/>
+</form>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $r): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Account Number:</div>
+                        <div><?php safer_echo($r["account_number"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Account:</div>
+                        <div><?php safer_echo($r["Account"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Owner:</div>
+                        <div><?php safer_echo($r["username"]); ?></div>
+                    </div>
+                    <div>
+                        <a type="button" href="test_edit_incubator.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                        <a type="button" href="test_view_incubator.php?id=<?php safer_echo($r['id']); ?>">View</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
---
+++ project/test_view_transcations.php
@@ -0,0 +1,50 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+//we'll put this at the top so both php block have access to it
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+?>
+
+<?php
+$result = [];
+if (isset($id)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT Transactions.id,Transactions.act_src_id,Transactions.act_dest_id,Transactions.amount, Users.username, Egg.name as egg FROM Transactions as Transactions JOIN Users on Transactions.user_id = Users.id LEFT JOIN Transactions Transaction on Transactions.id = Transactions.egg_id where Transcation.id = :id");
+    $r = $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$result) {
+        $e = $stmt->errorInfo();
+        flash($e[2]);
+    }
+}
+?>
+
+
+<h3>View Transaction</h3>
+<?php if (isset($result) && !empty($result)): ?>
+    <div class="card">
+        <div class="card-title">
+            <?php safer_echo($result["account_number"]); ?>
+        </div>
+        <div class="card-body">
+            <div>
+                <p>Stats</p>
+                <div>Account Type: <?php safer_echo($result["account_type"]); ?></div>
+                <div>Transaction Type: <?php safer_echo($result["action"]); ?> - <?php safer_echo($result["mod_max"]); ?></div>
+                <div>Transaction: <?php safer_echo($result["transaction"]); ?></div>
+                <div>Owned by: <?php safer_echo($result["username"]); ?></div>
+            </div>
+        </div>
+    </div>
+<?php else: ?>
+    <p>Error looking up id...</p>
+<?php endif; ?>
+<?php require(__DIR__ . "/partials/flash.php");
