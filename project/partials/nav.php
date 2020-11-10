6b9c5a8dbb7f66a6bd7d27527bbe721f40b5eb3e
dd482IT <dd482@njit.edu>
2020-11-10 18:15:20 -0500

    fix

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -18,8 +18,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
-        <li><a href="test_create_incubator.php">Create Transaction</a></li>
-        <li><a href="test_list_incubators.php">View Transaction</a></li>
+        <li><a href="test_create_transactions.php">Create Transaction</a></li>
+        <li><a href="test_list_transactions.php">View Transaction</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
