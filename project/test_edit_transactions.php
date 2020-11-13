2c3a09975fe4638a510c9a2a4f7a0dbf233c4360
dd482IT <dd482@njit.edu>
2020-11-13 16:32:28 -0500

    list stuff

--- project/test_edit_transactions.php
+++ project/test_edit_transactions.php
@@ -15,15 +15,10 @@ if (isset($_GET["id"])) {
 <?php
 //saving
 if (isset($_POST["save"])) {
-    $account_number=$_POST["account_number"];
-    $transaction =$_POST["transaction_id"];
     if($transaction <= 0){
         $transaction = null; 
     }
-    $source = $_POST["s_id"];
-    $destination = $_POST["d_id"];
-    $amount = $_POST["amount"];
-    $action  = $_POST["action"];
+    
     $user = get_user_id();
     $db = getDB();
     if (isset($id)) { //balance and trasnaction type
@@ -31,9 +26,6 @@ if (isset($_POST["save"])) {
         act_src_id=:s_id, act_dest_id=:d_id, action_type=:action where id=:id"); //check proper ID
         $r = $stmt->execute([
             ":amount" => $amount,
-            ":action" => $action, 
-        
-            ":id" => $transaction
         ]);
         if ($r) {
             flash("Updated successfully with id: " . $id);
@@ -65,16 +57,8 @@ $accounts = getDropDown();
 ?>
     <h3>Edit Transaction</h3>
     <form method="POST">
-        <label>Account Number</label>
-        <input name="account_number" value="<?php echo $result["account_number"]; ?>"/>
         <label>Transaction</label>
-        <select name="transaction_id" value="<?php echo $result["transaction_id"];?>" >
-            <option value="-1">None</option>
-            <?php foreach ($transaction as $transaction): ?>
-                <option value="<?php safer_echo($transaction["id"]); ?>" <?php echo ($result["transaction_id"] == $transaction["id"] ? 'selected="selected"' : ''); ?>
-                ><?php safer_echo($transaction["transaction_id"]); ?></option>
-            <?php endforeach; ?>
-        </select> 
+        <input type="number" name="transaction_id" value="<?php echo $result["transaction_id"];?>" >
         <label>Source Account</label>
             <select name="s_id">
                 <?php foreach($accounts as $row):?>
@@ -93,14 +77,6 @@ $accounts = getDropDown();
             </select>      
         <label>Amount</label> 
         <input type="number" name="amount" value="<?php echo $result["amount"]; ?>"/>
-        <label>Action</label> 
-        <input name="action" placeholder="deposit" value="<?php echo $result["action"]; ?>"/>
-                <select>
-                <option value = "deposit">desposit</option>
-                <option value =  "transfer">transfer</option>
-                <option value = "withdraw">withdraw</option>
-                </select> 
-        <input type="submit" name="save" value="Update"/>
     </form>
 
 
--- project/test_list_transcations.php
+++ project/test_list_transcations.php
@@ -49,6 +49,7 @@ if (isset($_POST["search"]) && !empty($query)) {
 <div class="results">
     <?php if (count($results2) > 0): ?>
         <div class="list-group">
+            <?php safer_echo($results2, true); ?>
             <?php foreach ($results2 as $r): ?>
                 <div class="list-group-item">
                     <div>
