72b99ef7e70b76944daf6f6f45c782fdc2c88fae
dd482IT <dd482@njit.edu>
2020-11-13 16:14:03 -0500

    list stuff

--- project/test_list_transcations.php
+++ project/test_list_transcations.php
@@ -72,6 +72,10 @@ if (isset($_POST["search"]) && !empty($query)) {
                         <div><?php safer_echo($r["amount"]); ?></div>
                     </div>
                     <div>
+                        <div>Transaction ID:</div>
+                        <div><?php safer_echo($r["id"]); ?></div>
+                    </div>
+                    <div>
                         <a type="button" href="test_edit_transactions.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
                         <a type="button" href="test_view_transactions.php?id=<?php safer_echo($r['id']); ?>">View</a>
                     </div>
