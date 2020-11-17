681b75b11cbc06c819381c1e15fbcdcfc0e19446
dd482IT <dd482@njit.edu>
2020-11-17 18:19:50 -0500

    View Transaction Fix

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -22,7 +22,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="test_list_transcations.php">View Transaction</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
-            <li><a href="profile.php">Profile</a></li>
+            <li><a href="profile.php">Profile</a></li>  
             <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
