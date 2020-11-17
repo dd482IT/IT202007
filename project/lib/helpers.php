fa9b79b2e46a1169bb283422c7f0c4f809eebec1
dd482IT <dd482@njit.edu>
2020-11-17 16:54:38 -0500

    View Transaction Fix

--- project/lib/helpers.php
+++ project/lib/helpers.php
@@ -36,7 +36,7 @@ function getURL($path) {
     if (substr($path, 0, 1) == "/") {
         return $path;
     }
-    return $_SERVER["CONTEXT_PREFIX"] . "/IT202/project/$path";
+    return $_SERVER["CONTEXT_PREFIX"] . "/IT202007/project/$path";
 }
 
 function get_email() {
