b93ef273cb1cb821fd2d5b612745c0e4318ef126
dd482IT <dd482@njit.edu>
2020-11-17 18:25:29 -0500

    View Transaction Fix

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,9 +1,9 @@
-<link rel="stylesheet" href="<?php echo getURL("static/css/styles.css");?>">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <?php
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
+<link rel="stylesheet" href="<?php echo getURL("static/css/styles.css");?>">
 <nav>
 <ul class="nav">
     <li><a href="home.php">Home</a></li>
