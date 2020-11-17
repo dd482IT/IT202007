6ce15d1f4b4420f5e5703250aa22c7d5d796ffe6
dd482IT <dd482@njit.edu>
2020-11-17 17:18:57 -0500

    View Transaction Fix

--- project/testFiles/test_create_transactions.php
+++ project/testFiles/test_create_transactions.php
@@ -3,7 +3,7 @@
 <?php
 if(!has_role("Admin")){
     flash("You dont have permission to access this page");
-    die(header(getURL("login.php")));
+    die(header(getURL("Location: login.php")));
 }
 ?>
 <?php
