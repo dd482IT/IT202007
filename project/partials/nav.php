2682d67c159a9f87fb34371b83ab05371292a0ee
dd482IT <dd482@njit.edu>
2020-11-17 19:04:50 -0500

    Final Fix

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,3 +1,4 @@
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <?php
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
@@ -12,14 +13,14 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
-            <li><a href="test_create_accounts.php">Create Account</a></li>
-            <li><a href="test_list_accounts.php">View Accounts</a></li>
+            <li><a href="<?php echo getURL("testFiles/test_create_accounts.php");?>">Create Account</a></li>
+            <li><a href="<?php echo getURL("testFiles/test_list_accounts.php");?>">View Accounts</a></li>
         <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
         <li><a href="<?php echo getURL("testFiles/test_create_transactions.php");?>">Create Transaction</a></li>
-        <li><a href="test_list_transcations.php">View Transaction</a></li>
+        <li><a href="<?php echo getURL("testFiles/test_list_transcations.php");?>">View Transaction</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>  
--- project/testFiles/test_edit_accounts.php
+++ project/testFiles/test_edit_accounts.php
@@ -1,10 +1,10 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<<?php require_once(__DIR__ . "/../partials/nav.php"); ?>
 <!-- ----------------------------------------------------------------------------------------------------------- -->
 <?php
 if (!has_role("Admin")) {
   //this will redirect to login and kill the rest of this script (prevent it from executing)
   flash("You don't have permission to access this page");
-  die(header("Location: login.php"));
+  die(header("Location: " . getURL("login.php")));
 }
 ?>
 <!-- ----------------------------------------------------------------------------------------------------------- -->
@@ -68,4 +68,4 @@ if(isset($id)){
   <input type="submit" name="save" value="Update"/>
 </form>
 <!-- ----------------------------------------------------------------------------------------------------------- -->
-<?php require(__DIR__ . "/partials/flash.php");
+<?php require(__DIR__ . "/../partials/flash.php");
