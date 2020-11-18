e1091645d8ab9ee5deeb4002f6a60eaba0554f47
dd482IT <dd482@njit.edu>
2020-11-17 19:10:27 -0500

    Final Fix

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -9,22 +9,18 @@ require_once(__DIR__ . "/../lib/helpers.php");
 <ul class="nav">
     <li><a href="home.php">Home</a></li>
     <?php if (!is_logged_in()): ?>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
+        <li><a href="<?php echo getURL("login.php");?>">Login</a></li>
+        <li><a href="<?php echo getURL("register.php");?>">Register</a></li>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
             <li><a href="<?php echo getURL("testFiles/test_create_accounts.php");?>">Create Account</a></li>
             <li><a href="<?php echo getURL("testFiles/test_list_accounts.php");?>">View Accounts</a></li>
         <?php endif; ?>
     <?php if (is_logged_in()): ?>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="logout.php">Logout</a></li>
+        <li><a href="<?php echo getURL("profile.php");?>">Profile</a></li>
+        <li><a href="<?php echo getURL("logout.php");?>">Logout</a></li>
         <li><a href="<?php echo getURL("testFiles/test_create_transactions.php");?>">Create Transaction</a></li>
         <li><a href="<?php echo getURL("testFiles/test_list_transcations.php");?>">View Transaction</a></li>
-    <?php endif; ?>
-    <?php if (is_logged_in()): ?>
-            <li><a href="profile.php">Profile</a></li>  
-            <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
 </nav>
