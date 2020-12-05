d904e6eceb331ac98328c0d3041b4660f9e96c81
dd482IT <dd482@njit.edu>
2020-12-05 16:42:05 -0500

    Default Value

--- project/accounts/my_accounts.php
+++ project/accounts/my_accounts.php
@@ -3,22 +3,109 @@
 ?>
 
 <?php
-  $user = get_user_id();
-  if(isset($user)){
-  $results = [];
-  $db = getDB();
-  $stmt = $db->prepare("SELECT Accounts.user_id as UserID, Accounts.id as AccID, account_number, account_type, balance FROM Accounts WHERE Accounts.user_id = :q LIMIT 5");
-  $r = $stmt->execute([":q" => $user]);
-    if($r){
-      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (!is_logged_in()) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You must be logged in to access this page");
+    die(header("Location: login.php"));
+}
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+}
+
+$page = 1;
+$per_page = 10;
+if(isset($_GET["page"])){
+    try {
+        $page = (int)$_GET["page"];
     }
-    else{
-      flash("There was a problem listing your accounts"); 
+    catch(Exception $e){
     }
-  }
+}
+
 ?>
 
-<div class="results">
+<?php
+    $user = get_user_id();
+    if(isset($user)){
+    $results = [];
+    $db = getDB();
+    $stmt = $db->prepare("SELECT count(*) as total from Accounts e LEFT JOIN Transactions i on e.id = i.act_src_id where e.user_id = :id");
+    $stmt->execute([":id"=>get_user_id()]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $total = 0;
+    if($result){
+        $total = (int)$result["total"];
+    }
+    $total_pages = ceil($total / $per_page);
+    $offset = ($page-1) * $per_page;
+
+
+
+    $stmt = $db->prepare("SELECT Accounts.user_id as UserID, Accounts.id as AccID, account_number, account_type, balance FROM Accounts WHERE Accounts.user_id = :q LIMIT :offset, :count");
+    $stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
+    $stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
+    $stmt->bindValue(":id", get_user_id());
+    $stmt->execute();
+    $e = $stmt->errorInfo();
+    if($e[0] != "00000"){
+        flash(var_export($e, true), "alert");
+    }
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+  }
+?>
+<div class="container-fluid">
+    <h3>My Accounts</h3>
+    <div class="row">
+    <div class="card-group">
+<?php if($results && count($results) > 0):?>
+    <?php foreach($results as $r):?>
+        <div class="col-auto mb-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <div class="card-title">
+                        <?php safer_echo($r["account_number"]);?>
+                    </div>
+                    <div class="card-text">
+                        <div>Current Balance: <?php safer_echo($r["balance"]); ?></div>
+                        <?php if(isset($r["inc"])):?>
+                            Account Type <?php safer_echo($r["account_type"]);?>
+                        <?php else:?>
+                            Not Set
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endforeach;?>
+
+<?php else:?>
+<div class="col-auto">
+    <div class="card">
+       You don't have any accounts
+    </div>
+</div>
+<?php endif;?>
+    </div>
+    </div>
+        <nav aria-label="My Accounts">
+            <ul class="pagination justify-content-center">
+                <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+                    <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+                </li>
+                <?php for($i = 0; $i < $total_pages; $i++):?>
+                <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+                <?php endfor; ?>
+                <li class="page-item <?php echo ($page+1) >= $total_pages?"disabled":"";?>">
+                    <a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+<?php require(__DIR__ . "/../partials/flash.php");
+
+/*<div class="results">
     <?php if (count($results) > 0): ?>
         <div class="list-group">
             <?php foreach ($results as $r): ?>
@@ -45,4 +132,4 @@
         <p>No results</p>
     <?php endif; ?>
 </div>
-<?php require(__DIR__ . "/../partials/flash.php");
+*/
