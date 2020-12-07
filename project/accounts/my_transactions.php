db443a8cca469ef1fd72198d65884c6e4c08c11e
dd482IT <dd482@njit.edu>
2020-12-07 14:52:15 -0500

    Pagination fix

--- project/accounts/my_transactions.php
+++ project/accounts/my_transactions.php
@@ -168,7 +168,7 @@ if (isset($accID) && !empty($accID)) {
                     <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
                 </li>
                 <?php for($i = 0; $i < $total_pages; $i++):?>
-                <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+                <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?id=<?php echo ($accID);?>&page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
                 <?php endfor; ?>
                 <li class="page-item <?php echo ($page+1) >= $total_pages?"disabled":"";?>">
                     <a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
