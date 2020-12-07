03383235a0b77075a385797659cd3860f3b479ae
dd482IT <dd482@njit.edu>
2020-12-06 19:56:54 -0500

    Implementing Send money

--- project/accounts/send.php
+++ project/accounts/send.php
@@ -10,7 +10,7 @@ $accounts = getDropDown();
     <h3 class="text-center"><strong>Send Money</strong></h3> 
     <hr>
     <form align="center" method="POST">     
-        <label>Source User Account</label placeholder="0">
+        <label>Source Account</label placeholder="0">
             <select name="s_id">
             <?php foreach($accounts as $row):?>
                 <option value="<?php echo $row["id"];?>"> 
@@ -19,9 +19,9 @@ $accounts = getDropDown();
             <?php endforeach;?>
             </select>
         <div id="transfer">
-            <label>Destination User Account # Last 4 </label>
+            <label>User Account # Last 4 </label>
             <input type="text" name="destID" placeholder="Search..">
-            <label>Destination User Last Name</label>
+            <label>User Last Name</label>
             <input type="text" name="destName" placeholder="Search..">
         </div>
         <label>Amount</label> 
@@ -37,12 +37,40 @@ $accounts = getDropDown();
     if(isset($_POST["save"])){
         //$account_type = $_POST["account_type"];
         $source = $_POST["s_id"]; //ACCOUNT 1 
-        $destination = $_POST["d_id"]; //ACCOUNT 2 
+        $destLast4 = $_POST["destID"]; //ACCOUNT 2 
+        $destLastName = $_POST["destName"];
         $amount = $_POST["amount"];
-        $action  = $_POST["action"];// WITHDRAWAL, DESPOIT, TRANSFER
         $memo = $_POST["memo"];
         $user = get_user_id();
         $db = getDB();
+        $destUserID;
+        $destAccID;
+        $destination;
+        $destAccNum;
+        //SELECT Accounts.id, user_id FROM Accounts WHERE account_number LIKE '%8068'
+
+        //FINDS THE USER ID BY COMPARING THE NAME 
+        $stmt=$db->prepare("SELECT Users.id as userID FROM Users WHERE Users.lastName LIKE :q");
+        $results = $stmt->execute([":q"=> $destLastName]);
+        $r = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($r){
+        $destUserID = $r["userID"];
+        }
+        else{
+          flash("Error Pulling destUserID");
+        }
+        //FINDS THE ACCOUNT NUMBER AND ID BY COMPARING THE LAST 4, NAME NEEDS TO BE COMPARED FIRST^^^
+        $stmt=$db->prepare("SELECT Accounts.id as accID FROM Accounts WHERE Accounts.user_id = :userID AND account_number as accNum LIKE :q");
+        $results = $stmt->execute([":userID"=> $destUserID, ":q"=> "%$destLast4"]);
+        $r = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($r){
+          $destination = $r["accID"];
+          $destAccNum = $r["accNum"];
+        }
+        else{
+          flash("Error Pulling destAccNum and destination");
+        }
+
 
         $stmt=$db->prepare("SELECT balance FROM Accounts WHERE Accounts.id = :q");
         $results = $stmt->execute(["q"=> $source]);
@@ -60,6 +88,9 @@ $accounts = getDropDown();
         elseif($amount > $balance){
           flash("Balance Too Low");
         } 
+        else{
+          flash("Other Error")
+        }
     }
    
 
