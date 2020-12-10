8018f6a7294adc06db5a1f8bb927fa1991b56d20
dd482IT <dd482@njit.edu>
2020-12-10 18:29:32 -0500

    Hiding Email

--- project/profile.php
+++ project/profile.php
@@ -125,6 +125,10 @@
         }
     }
 
+    $isViewed = false;
+    $myProfile = ($id == get_user_id());
+    $profileData = [];
+
     $stmt = $db->prepare("SELECT email, username, firstName, lastName, visibility from Users WHERE id = :id LIMIT 1");
             $stmt->execute([":id" => $id]);
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -144,31 +148,46 @@
                 die(header("Location: home.php"));
             }
 
+    if($result["visibility"] == "public" || $id == get_user_id()){
+        $profileData = $result;
+        $isViewed = true;
+    }
+    else{
+        flash("This is private profile");
+        die(header("Location: home.php"));
+    }
+
 ?>
     <div class="profile">
         <form method="POST">
-        <text><Strong>Current Email: </Strong></text> <?php if($result["visibility"] == "private"){safer_echo(get_email());}; ?>
-        <text><Strong>Current Username:</Strong></text> <?php safer_echo(get_username()); ?>   
-        <text><Strong>Role:</Strong></text> 
-            <label for="email">Email</label>
-            <input type="email" name="email" value="<?php safer_echo(get_email()); ?>"/>
-            <label for="username">Username</label>
-            <input type="text" maxlength="60" name="username" value="<?php if($result["visibility"] == "private"){safer_echo(get_email());}; ?>"/>
+            <?php foreach($profileData as $record):?>
+            <?php if(!$isViewed):?>
+                <input type="password" name="passwordCheck" <?php echo (!$myProfile?"disabled='disabled'":"");?>/>
+            <?php endif;?>
+            <?php if($isViewed):?>    
+            <text><Strong>Current Email: </Strong></text> <?php safer_echo(get_email()); ?>
+            <text><Strong>Current Username:</Strong></text> <?php safer_echo(get_username()); ?>   
+            <text><Strong>Role:</Strong></text> 
+                <label for="email">Email</label>
+                <input type="email" name="email" value="<?php safer_echo(get_email()); ?>"/>
+                <label for="username">Username</label>
+                <input type="text" maxlength="60" name="username" value="<?php safer_echo(get_email());?>"/>
 
-            <label for="firstName">First Name</label>
-            <input type="text" maxlength="60" name="firstName" value="<?php safer_echo(get_firstName()); ?>"/>
-            <label for="lastName">Last Name</label>
-            <input type="text" maxlength="60" name="lastName" value="<?php safer_echo(get_lastName()); ?>"/>
-            <!-- DO NOT PRELOAD PASSWORD-->
-            <label for="ogpw">Current Password</label> <!-- Added by dan -->
-            <input type="password" name="original" id="ogpw" /> <!-- Added by dan -->
-            <label for="pw">New Password</label>
-            <input type="password" name="password"/>
-            <label for="cpw">Confirm Password</label>
-            <input type="password" name="confirm"/>
-            <input type="checkbox" name="visbility" <?php echo $result["visibility"] == "private"?"checked='checked'":"";?> />    
-            <label for="visbility">Private Profile</label><br>
-            <input class="btn btn-primary" type="submit" name="saved" value="Save Profile"/>
-        </form>
+                <label for="firstName">First Name</label>
+                <input type="text" maxlength="60" name="firstName" value="<?php safer_echo(get_firstName()); ?>"/>
+                <label for="lastName">Last Name</label>
+                <input type="text" maxlength="60" name="lastName" value="<?php safer_echo(get_lastName()); ?>"/>
+                <!-- DO NOT PRELOAD PASSWORD-->
+                <label for="ogpw">Current Password</label> <!-- Added by dan -->
+                <input type="password" name="original" id="ogpw" /> <!-- Added by dan -->
+                <label for="pw">New Password</label>
+                <input type="password" name="password"/>
+                <label for="cpw">Confirm Password</label>
+                <input type="password" name="confirm"/>
+                <input type="checkbox" name="visbility" <?php echo $result["visibility"] == "private"?"checked='checked'":"";?> />    
+                <label for="visbility">Private Profile</label><br>
+                <input class="btn btn-primary" type="submit" name="saved" value="Save Profile"/>
+            </form>
+            <?php endif;?>
     </div>
     <?php require(__DIR__ . "/partials/flash.php");
