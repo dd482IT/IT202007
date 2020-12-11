5fed3337444b8188c664824dc4c699262b6a1333
dd482IT <dd482@njit.edu>
2020-12-10 19:36:57 -0500

    Adding an admin page

--- project/testFiles/admin_page.php
+++ project/testFiles/admin_page.php
@@ -1,11 +1,1 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); 
-      require_once(__DIR__ . "/../lib/helpers.php");
-?>
-
-<?php
-  if (!has_role("Admin")) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
-    flash("You don't have permission to access this page");
-    die(header("Location: " . getURL("login.php")));
-  }
-?>
+<?php require_once(__DIR__ . "/partials/nav.php");?>
