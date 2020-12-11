7922e5da1c54d22e24dc0523017b9d2a4dfeaa05
dd482IT <dd482@njit.edu>
2020-12-10 19:35:59 -0500

    Adding an admin page

--- project/testFiles/admin_page.php
+++ project/testFiles/admin_page.php
@@ -9,18 +9,3 @@
     die(header("Location: " . getURL("login.php")));
   }
 ?>
-
-
-
-<h3 class="text-center"><strong>Search for user</strong></h3> 
-    <hr>
-    <form align="center" method="POST">     
-        <div id="search">
-            <label>User First Name</label>
-            <input type="text" name="firstName" placeholder="Search.." required>
-            <label>User Last Name</label>
-            <input type="text" name="lastName" placeholder="Search.." required>
-        </div>
-        <input class="btn btn-primary" type ="submit" name="search" value="search"/>
-    <hr> 
-    </form> 
