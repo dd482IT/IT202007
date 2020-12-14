0d2ede6853f6c978c89aa7fd5795583b305e3d74
dd482IT <dd482@njit.edu>
2020-12-13 22:49:43 -0500

    Adding HelperMethod and SQL

--- project/accounts/create_accounts.php
+++ project/accounts/create_accounts.php
@@ -11,9 +11,9 @@
 <form method="POST">
   <label>Account Type</label>
   <select name="account_type">
-    <option value = "checking">checking</option>
-    <option value =  "saving">saving</option>
-    <option value = "loan">loan</option>
+    <option value ="checking">checking</option>
+    <option value ="saving">saving</option>
+    <option value ="loan">loan</option>
   </select>
   <label>Balance</label>
   <input type="number" min="5.00" name="balance" value="<?php echo $result["balance"];?>" />
