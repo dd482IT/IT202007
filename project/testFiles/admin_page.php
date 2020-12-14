dc2c4d7a4caee5df62d215614ab445d84170c9c9
dd482IT <dd482@njit.edu>
2020-12-14 14:14:19 -0500

    Adding search account number

--- project/testFiles/admin_page.php
+++ project/testFiles/admin_page.php
@@ -49,3 +49,35 @@ if(isset($_POST["search"])){
       <a type="button" class="btn btn-primary" name="search" href="<?php echo getURL("profile.php?id=" . $r["userID"] . "&viewer=" . $id)?>">Go To <?php echo ($r["firstName"] . " " .$r["lastName"]) ?> Profile</a>
     </div>   
   <?php endforeach; ?>
+<hr> 
+
+
+<?php 
+$results =[];
+if(isset($_POST["search2"])){
+  $db = getDB();
+  $account_number = $_POST["account_number"];
+  $destUserID = null;
+
+  $stmt=$db->prepare("SELECT * from Accounts WHERE account_number = :q");
+  $r = $stmt->execute([":q"=> $lastName,]);
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+?> 
+
+<h3 class="text-center"><strong>Search for user</strong></h3> 
+  <hr>
+  <form align="center" method="POST">     
+        <div id="search">
+            <label>User Account Number</label>
+            <input type="text" name="account_number" placeholder="Search.." required>
+        </div>
+        <input class="btn btn-primary" type ="submit" name="search" value="find profile"/>
+  </form> 
+  <?php foreach($results as $r):?>
+    <div align="center">
+      <a type="button" class="btn btn-primary" name="search2" href="<?php echo getURL("profile.php?id=" . $r["userID"] . "&viewer=" . $id)?>">Go To <?php echo ($r["firstName"] . " " .$r["lastName"]) ?> Profile</a>
+    </div>   
+  <?php endforeach; ?>
+<hr> 
