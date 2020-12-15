39fe82b57bd6874a93aa51fd3990ca2f957da615
dd482IT <dd482@njit.edu>
2020-12-15 14:39:12 -0500

    Adding apy display to accounts_list

--- project/accounts/close_account.php
+++ project/accounts/close_account.php
@@ -8,4 +8,29 @@
 ?>
 
 
+
+
+<form method="POST">
+  <label>Are you deactivating the account number <?php echo $account_number;?> </label>
+  <label>Please Enter the account number Type</label>
+  <input type="number" name="account_number"/>
+  <input class="btn btn-primary" type="submit" name="save" value="Create"/>
+</form>
+
+<?php
+  getDB();
+  $stmt = $db->prepare("SELECT account_number, balance, active FROM Accounts Accounts.id = :id");
+  $stmt->execute([":id"=>get_user_id()]);
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  $account_number = $result["account_number"];
+
+
+
+
+
+?>
+
+
+
+
 <?php require(__DIR__ . "/../partials/flash.php");
