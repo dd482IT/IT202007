16f19242b600537557bbd09be56423498d42f6b9
dd482IT <dd482@njit.edu>
2020-12-15 13:11:53 -0500

    Fixing Flash

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -29,18 +29,16 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="<?php echo getURL("accounts/create_transactions.php");?>">Make a Transaction</a></li>
         <li><a href="<?php echo getURL("accounts/send.php");?>">Send Money</a></li>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Loan
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="<?php echo getURL("accounts/manage_loan.php");?>">Action</a>
+                <a class="dropdown-item" href="<?php echo getURL("accounts/apply_for_loan.php");?>">Another action</a>
+                </div>
+            </li>
         </div>
-      </li>
-    </div>
     <?php endif; ?>
 </ul>
 </nav>
